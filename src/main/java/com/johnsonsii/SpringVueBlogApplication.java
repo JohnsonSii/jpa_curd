d2f27c65033ae4a11b1eac76df3705bbe05b564f
Johnson Sii <2933764599@qq.com>
2022-06-10 01:18:07 +0800

    Initial commit

---
+++ src/main/java/com/johnsonsii/SpringVueBlogApplication.java
@@ -0,0 +1,13 @@
+package com.johnsonsii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVueBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringVueBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/johnsonsii/SpringVueBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnsonsii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVueBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
